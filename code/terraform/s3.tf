1a5bbd15167f6625f7252bd8b36ad2a642dab707
fumantsu <alexandros.naoum@gmail.com>
2025-07-13 14:35:00 +0200

    fix s3 encryption type

--- code/terraform/s3.tf
+++ code/terraform/s3.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_storage
   rule {
     bucket_key_enabled = true
     apply_server_side_encryption_by_default {
-        
+
       sse_algorithm = "AES256"
     }
   }
