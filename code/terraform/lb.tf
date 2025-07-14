3ef1e2ff8235362beaac168c9a59e0693fc3e005
fumantsu <alexandros.naoum@gmail.com>
2025-07-15 01:41:42 +0200

    code for load balancer

--- code/terraform/lb.tf
+++ code/terraform/lb.tf
@@ -1,35 +1,45 @@
-# resource "aws_lb" "web" {
-#   name               = "web-server-lb"
-#   load_balancer_type = "application"
+resource "aws_lb" "web" {
+  name               = "web-server-lb"
+  load_balancer_type = "application"
+  subnets            = [data.aws_subnets.main_vpc.ids[0], data.aws_subnets.main_vpc.ids[1]]
+  security_groups    = [aws_security_group.lb.id]
+  tags = {
+    name = "webservers-lb"
+  }
+}
 
-#   tags = {
-#     name = "web-server-lb"
-#   }
-# }
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-# resource "aws_lb_listener" "web_listener" {
-#   load_balancer_arn = aws_lb.web.arn
-#   port              = "80"
-#   protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_group.arn
+  }
+  tags = {
+    name = "webservers-http-listener"
+  }
+}
 
-#   default_action {
-#     type = "forward"
-#   }
-#   tags = {
-#     name = "webservers-http-listener"
-#   }
-# }
 
-# resource "aws_lb_target_group" "web_target_group" {
-#   name                          = "web-servers-target-group"
-#   port                          = 80
-#   protocol                      = "HTTP"
-#   load_balancing_algorithm_type = "round-robin"
-#   #   stickiness {
-#   #     enabled = true
-#   #     type = lb_cookie
-#   #   }
-#   tags = {
-#     name = "webservers-target-group"
-#   }
-# }
+resource "aws_lb_target_group" "web_target_group" {
+  name                          = "web-servers-target-group"
+  port                          = 80
+  protocol                      = "HTTP"
+  load_balancing_algorithm_type = "round_robin"
+  vpc_id                        = local.vpc_id
+  stickiness {
+    enabled = false
+    type    = "lb_cookie"
+  }
+  tags = {
+    name = "webservers-target-group"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_servers" {
+  count            = local.server_amount
+  target_group_arn = aws_lb_target_group.web_target_group.arn
+  target_id        = aws_instance.web_server[count.index].id
+}
--- code/terraform/sg.tf
+++ code/terraform/sg.tf
@@ -1,3 +1,35 @@
+resource "aws_security_group" "lb" {
+  name   = "lb"
+  vpc_id = local.vpc_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lb_incoming_http" {
+  security_group_id = aws_security_group.lb.id
+
+  cidr_ipv4   = "213.220.234.36/32"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lb_incoming_https" {
+  security_group_id = aws_security_group.lb.id
+
+  cidr_ipv4   = "213.220.234.36/32"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "to_web_server" {
+  security_group_id = aws_security_group.lb.id
+
+  cidr_ipv4   = data.aws_vpc.main_vpc.cidr_block
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
 resource "aws_security_group" "web_server" {
   name   = "webserver"
   vpc_id = local.vpc_id
@@ -6,19 +38,19 @@ resource "aws_security_group" "web_server" {
 resource "aws_vpc_security_group_ingress_rule" "web_server_incoming" {
   security_group_id = aws_security_group.web_server.id
 
-  cidr_ipv4   = "213.220.234.36/32" #todo add lb ip
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+  referenced_security_group_id = aws_security_group.lb.id
+  from_port                    = 80
+  ip_protocol                  = "tcp"
+  to_port                      = 80
 }
 
 resource "aws_vpc_security_group_egress_rule" "web_server_out_db" {
   security_group_id = aws_security_group.web_server.id
 
-  cidr_ipv4   = data.aws_vpc.main_vpc.cidr_block # todo more specific subnet
-  from_port   = 5432
-  ip_protocol = "tcp"
-  to_port     = 5432
+  cidr_ipv4   = data.aws_vpc.main_vpc.cidr_block
+  from_port   = -1
+  ip_protocol = "ALL"
+  to_port     = -1
 }
 resource "aws_security_group" "db" {
   name   = "db"
@@ -33,6 +65,17 @@ resource "aws_vpc_security_group_ingress_rule" "db_incoming" {
   ip_protocol                  = "tcp"
   to_port                      = 5432
 }
+
+resource "aws_vpc_security_group_egress_rule" "db_out" {
+  security_group_id = aws_security_group.db.id
+
+  cidr_ipv4   = data.aws_vpc.main_vpc.cidr_block
+  from_port   = -1
+  ip_protocol = "ALL"
+  to_port     = -1
+}
+
+
 resource "aws_security_group" "mgmt" {
   name   = "mgmt"
   vpc_id = local.vpc_id
