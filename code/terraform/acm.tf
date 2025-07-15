15d70cdfbe3a74546624185cd54b926148d479e8
fumantsu <alexandros.naoum@gmail.com>
2025-07-15 08:49:07 +0200

    sync

--- code/terraform/acm.tf
+++ code/terraform/acm.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "lab" {
-  domain_name               = "anaoum-lab.com"
-  key_algorithm             = "RSA_2048"
-  region                    = "eu-west-1"
-  validation_method         = "DNS"
+  domain_name       = "anaoum-lab.com"
+  key_algorithm     = "RSA_2048"
+  region            = "eu-west-1"
+  validation_method = "DNS"
 }
--- code/terraform/route53.tf
+++ code/terraform/route53.tf
@@ -1,13 +1,13 @@
 resource "aws_route53_zone" "lab" {
-  comment           = "Temporary hosted zone"
-  name              = "anaoum-lab.com"
+  comment = "Temporary hosted zone"
+  name    = "anaoum-lab.com"
 }
 
 resource "aws_route53_record" "lab_dns_validation" {
 
-  name                             = "_c2a4f41a9d909365e129ce7e41c9917f.anaoum-lab.com"
-  records                          = ["_4ae4d41a483715c99a3045d61247d42e.xlfgrmvvlj.acm-validations.aws."]
-  ttl                              = 300
-  type                             = "CNAME"
-  zone_id                          = aws_route53_zone.lab.id
+  name    = "_c2a4f41a9d909365e129ce7e41c9917f.anaoum-lab.com"
+  records = ["_4ae4d41a483715c99a3045d61247d42e.xlfgrmvvlj.acm-validations.aws."]
+  ttl     = 300
+  type    = "CNAME"
+  zone_id = aws_route53_zone.lab.id
 }
